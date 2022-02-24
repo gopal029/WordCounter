aba4de06d47c257c6870f05f55df8d018725279f
gopal <gopalthakur1129@gmail.com>
2022-02-24 16:12:29 +0530

    initial commit

---
+++ counter/urls.py
@@ -0,0 +1,10 @@
+from typing import Pattern
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('count',views.count,name='count'),
+    path('register',views.register,name='register'),
+    path('login',views.login, name='login'),
+    ]
